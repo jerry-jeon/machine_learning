29919ecfe93d290f50f819c37d20f1ada0f6c353
sorrowbeaver <jkj8790@gmail.com>
2016-10-26 02:22:00 +0900

    refactoring code

--- hw.py
+++ hw.py
@@ -5,12 +5,47 @@ import numpy as np
 # consider assume class size is two.
 ATTRIBUTE_SIZE = 13
 CLS_SIZE = 2
-class Machine:
+class PredictResult:
     def __init__(self):
-        self.training_data = []
-        self.cov_mat = []
-        self.cls_size = [0] * CLS_SIZE
+        self.true_positive = self.true_negative = self.false_positive = self.false_negative = 0
 
+    def addData(self, predict, actual_cls):
+        if predict == actual_cls:
+            if predict == 0:
+                self.true_positive += 1
+            else:
+                self.true_negative += 1
+        else:
+            if predict == 0:
+                self.false_negative += 1
+            else:
+                self.false_positive += 1
+
+    def empiricalError(self):
+        return self.false_negative + self.false_positive
+
+    def size(self):
+        return self.true_positive + self.true_negative + self.false_positive + self.false_negative
+
+    def fp_rate(self):
+        return self.false_positive / (self.false_positive + self.true_negative * 1.0)
+
+    def tp_rate(self):
+        return self.true_positive / (self.true_positive + self.false_negative * 1.0)
+
+    def print(self):
+        print("Empirical error : " + str(self.empiricalError()))
+        print()
+        print("Confusion Matrix")
+        print("--------------------------------------------")
+        print("True positive : " + str(self.true_positive))
+        print("True negative : " + str(self.true_negative))
+        print("False positive : " + str(self.false_positive))
+        print("False negative : " + str(self.false_negative))
+
+#roc curve - x cord is fp-rate y corod - tp rate
+
+class Machine:
     def is_valid(self, data):
         if len(data) > 13:
             return True
@@ -19,89 +54,95 @@ class Machine:
 
 # hmm.. I think parameter should be detail
     def learnFile(self, file):
+        training_data, sum, cls_size = self.fileToData(file)
+        mean = self.calculateMean(sum, cls_size)
+        cov_mat = self.calculateCovarianceMatrix(mean, training_data, cls_size)
+        prior = [cls_size[cls] / (len(training_data) * 1.0) for cls in range(CLS_SIZE)]
+
+        self.discriminant = self.makeDiscriminant(cov_mat, mean, prior)
+
+        self.roc_discriminants = []
+        roc_priors = [0.00001, 0.02, 0.3, 0.8, 0.99];
+        for roc_prior in roc_priors:
+            self.roc_discriminants.append(self.makeDiscriminant(cov_mat, mean, roc_prior))
+
+
+    def fileToData(self, file):
+        training_data = []
+        cls_size = [0] * CLS_SIZE
         data_lines = file.readlines()
         sum = np.full((CLS_SIZE, ATTRIBUTE_SIZE), 0.0)
         for event in data_lines:
             data_line = event.split()
             if self.is_valid(data_line):
-                # TODO rename ho
                 data = {
                     'cls': int(data_line.pop()),
                     'data': np.array([float(i) for i in data_line]),
                 }
                 sum[data['cls']] = np.add(sum[data['cls']], data['data'])
-                self.cls_size[data['cls']] += 1
-                self.training_data.append(data)
-
-        self.calculateMean(sum)
-        self.calculateCovarianceMatrix()
+                cls_size[data['cls']] += 1
+                training_data.append(data)
+        return training_data, sum, cls_size
         
-    def calculateParameters(self):
-        self.calculateVariance()
 
-    def calculateMean(self, sum):
+    def calculateMean(self, sum, cls_size):
         for cls, cls_sum in enumerate(sum):
-            cls_sum /= self.cls_size[cls]
+            cls_sum /= cls_size[cls]
 
-        self.mean = np.mat(sum).T
+        return np.mat(sum).T
 
-    def calculateCovarianceMatrix(self):
+    def calculateCovarianceMatrix(self, mean, training_data, cls_size):
+        cov_mat = []
         sum = [np.full((ATTRIBUTE_SIZE, ATTRIBUTE_SIZE), 0.0)] * CLS_SIZE
-        for data in self.training_data:
+        for data in training_data:
             sum[data['cls']] += np.mat(data['data']).T * np.mat(data['data'])
 
         for i in range(CLS_SIZE):
-            sum[i] = sum[i] / self.cls_size[i]
+            sum[i] = sum[i] / cls_size[i]
 
 
         for cls in range(CLS_SIZE):
-            mean = self.mean[:,cls] * self.mean[:,cls].T
-            self.cov_mat.append(sum[cls] - mean)
+#TODO
+            yo = mean[:,cls] * mean[:,cls].T
+            cov_mat.append(sum[cls] - yo)
+        
+        return cov_mat
 
-    def g(self, x, cls):
-        w_1 = -0.5 * self.cov_mat[cls].I
-        w_2 = self.cov_mat[cls].I * self.mean[:,cls]
-        w_3 = -0.5 * self.mean[:,cls].T * self.cov_mat[cls].I * self.mean[:,cls] - 0.5*log(np.linalg.det(self.cov_mat[cls])) + log(self.cls_size[cls] / (len(self.training_data) * 1.0))
-        result = np.mat(x) * w_1 * np.mat(x).T + w_2.T * np.mat(x).T + w_3
+    def makeDiscriminant(self, cov_mat, mean, prior):
+        def g(x, cls):
+            w_1 = -0.5 * cov_mat[cls].I
+            w_2 = cov_mat[cls].I * mean[:,cls]
+            w_3 = -0.5 * mean[:,cls].T * cov_mat[cls].I * mean[:,cls] - 0.5*log(np.linalg.det(cov_mat[cls])) + log(prior[cls])
+            result = np.mat(x) * w_1 * np.mat(x).T + w_2.T * np.mat(x).T + w_3
 
-        return result[0]
+            return result[0]
 
-    def predict(self, data):
+        return g
+
+    def predict(self, data, discriminant):
         results = []
         for i in range(CLS_SIZE):
-            results.append(self.g(data, i))
+            results.append(discriminant(data, i))
 
         return results.index(max(results))
-
-
-    def predictFile(self, file):
-        true_positive = true_negative = false_positive = false_negative = size = 0
-        data_lines = file.readlines()
+    
+    def predictDataLines(self, data_lines, discriminant):
+        predictResult = PredictResult()
         for event in data_lines:
             data_line = event.split()
             if self.is_valid(data_line):
-                size += 1
                 actual_cls = int(data_line.pop())
-                predict = self.predict(np.array([float(i) for i in data_line]))
-                if predict == actual_cls:
-                    if predict == 0:
-                        true_positive += 1
-                    else:
-                        true_negative += 1
-                else:
-                    if predict == 0:
-                        false_negative += 1
-                    else:
-                        false_positive += 1
-                
-        print("Empirical error : " + str(false_negative + false_positive))
-        print()
-        print("Confusion Matrix")
-        print("--------------------------------------------")
-        print("True positive : " + str(true_positive))
-        print("True negative : " + str(true_negative))
-        print("False positive : " + str(false_positive))
-        print("False negative : " + str(false_negative))
+                predict = self.predict(np.array([float(i) for i in data_line]), discriminant)
+                predictResult.addData(predict, actual_cls)
+
+        return predictResult
+
+
+    def predictFile(self, file):
+        data_lines = file.readlines()
+
+        return self.predictDataLines(data_lines, self.discriminant)
+
 
 
 machine = Machine()
@@ -110,4 +151,5 @@ with open('data/trn.txt') as file:
 
 test_datas = []
 with open('data/tst.txt') as file:
-    machine.predictFile(file)
+    predictResult = machine.predictFile(file)
+    predictResult.print()
