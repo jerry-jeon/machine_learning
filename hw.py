5341eacc69ae1cdb65250c69e143b4cc751578c6
sorrowbeaver <jkj8790@gmail.com>
2016-11-28 12:44:30 +0900

    one hidden layer with n node

--- hw.py
+++ hw.py
@@ -1,6 +1,7 @@
-from math import log, sqrt
+from math import log, sqrt, exp
 from operator import add
 import numpy as np
+import sys
 
 #consider expand abstract attribute size, class size
 ATTRIBUTE_SIZE = 13
@@ -8,11 +9,55 @@ CLS_SIZE = 2
 logging = False
 
 class Machine:
+
     def is_valid(self, data):
         if len(data) > 13:
             return True
         else:
             return False
+
+    def predictFile(self, file, withRoc):
+        roc_file = open("roc.txt", "w")
+        data_lines = file.readlines()
+        original = self.predictDataLines(data_lines)
+        original.print()
+        original.printRocPoint()
+
+        if withRoc:
+            print()
+            print("Give threshold for draw roc curve....")
+            print()
+
+            for i in range(200):
+                threshold = -10 + (i / 10)
+                print("threshold : " + str(threshold))
+                result = self.predictDataLines(data_lines, threshold)
+                if result.is_EER():
+                    EER = result
+                result.printRocPoint()
+                roc_file.write(str(result.fp_rate()) + "\t" + str(result.tp_rate()) + "\n")
+                print()
+
+            roc_file.close()
+
+            try:
+                print("Equal error rate")
+                EER.printRocPoint()
+            except:
+                print("Program can't find EER")
+
+    def predictDataLines(self, data_lines, threshold = 0):
+        predictResult = PredictResult()
+        for event in data_lines:
+            data_line = event.split()
+            if self.is_valid(data_line):
+                actual_cls = int(data_line.pop())
+                predict = self.predict(np.array([float(i) for i in data_line]), threshold)
+                predictResult.addData(predict, actual_cls)
+
+        return predictResult
+
+class BayesMachine(Machine):
 
     def learnFile(self, file):
         training_data, sum, cls_size, trans = self.fileToData(file)
@@ -73,47 +118,6 @@ class Machine:
 
         return g
 
-    def predictFile(self, file):
-        roc_file = open("roc.txt", "w")
-        data_lines = file.readlines()
-        original = self.predictDataLines(data_lines)
-        original.print()
-        original.printRocPoint()
-
-        print()
-        print("Give threshold for draw roc curve....")
-        print()
-
-        for i in range(200):
-            threshold = -10 + (i / 10)
-            print("threshold : " + str(threshold))
-            result = self.predictDataLines(data_lines, threshold)
-            if result.is_EER():
-                EER = result
-            result.printRocPoint()
-            roc_file.write(str(result.fp_rate()) + "\t" + str(result.tp_rate()) + "\n")
-            print()
-
-        roc_file.close()
-
-        try:
-            print("Equal error rate")
-            EER.printRocPoint()
-        except:
-            print("Program can't find EER")
-
-
-    def predictDataLines(self, data_lines, threshold = 0):
-        predictResult = PredictResult()
-        for event in data_lines:
-            data_line = event.split()
-            if self.is_valid(data_line):
-                actual_cls = int(data_line.pop())
-                predict = self.predict(np.array([float(i) for i in data_line]), threshold)
-                predictResult.addData(predict, actual_cls)
-
-        return predictResult
-
     def predict(self, data, threshold):
         positive = self.discriminant(data, 1)
         negative = self.discriminant(data, 0)
@@ -122,6 +126,102 @@ class Machine:
             return 1
         else:
             return 0
+
+class DeepLearningMachine(Machine):
+
+    def __init__(self):
+        self.epoch = 0
+    
+    def beginningWeight(self, row, col):
+        return np.random.uniform(-0.01, 0.01, (row, col)) 
+
+    def converge(self, delta = 0):
+        self.epoch += 1
+        if self.epoch > 10:
+            return True
+        else:
+            return False
+        '''
+                    if predelta == (delta  > 0):
+                        change += 1
+                    else:
+                        change = 0
+                        
+                    predelta = delta > 0
+                    '''
+
+    def learnFile(self, file):
+        training_data = self.fileToData(file)
+
+        hid_node = 2
+        w = self.beginningWeight(hid_node, ATTRIBUTE_SIZE)
+        v = self.beginningWeight(1, hid_node)
+        
+        eta = 0.001 # learning rate
+        activation = self.linear;
+        h = np.full((1, hid_node), 0.0)
+        z = np.full((1, hid_node), 0.0)
+
+        while not self.converge():
+            for data in training_data:
+                for h in range(hid_node):
+                    #print(-(np.mat(w[h]) * np.mat(data['data']).T))
+                    try:
+                        z[:,h] = 1.0 / (1 + exp(-(np.mat(w[h]) * np.mat(data['data']).T)))
+                    except:
+                        print(-(np.mat(w[h]) * np.mat(data['data']).T))
+
+                y = np.mat(v) * np.mat(z).T
+
+                v_delta = eta * (data['cls'] - y) * np.mat(z)
+
+                for h in range(hid_node):
+                    w_delta = eta * ((data['cls'] - y) * v[:,h]) * z[:,h] * (1 - z[:,h]) * np.mat(data['data'].T)
+
+                v += v_delta
+                for h in range(hid_node):
+                    w[:h,] += w_delta
+
+        def g(x):
+            return np.mat(v) * np.mat(w) * np.mat(x).T
+
+        self.discriminant = g
+
+    def predict(self, data, threshold):
+        result = self.discriminant(data)
+
+        if result + threshold > 0:
+            return 1
+        else:
+            return 0
+
+    def linear(self, O, cls):
+        return O[cls]
+
+    def sigmoid(self, O, cls):
+        return 1.0 / (1.0 + exp(O[cls]))
+
+    def softmax(self, O, cls):
+        denominator = 0.0
+        for cls in range(CLS_SIZE):
+            denominator += exp(O[cls])
+        return exp(O[cls]) / denominator
+
+    def fileToData(self, file):
+        training_data = []
+        cls_size = [0] * CLS_SIZE
+        data_lines = file.readlines()
+
+        for event in data_lines:
+            data_line = event.split()
+            if self.is_valid(data_line):
+                data = {
+                    'cls': int(data_line.pop()),
+                    'data': np.array([float(i) for i in data_line]),
+                }
+                training_data.append(data)
+
+        return training_data
 
 class PredictResult:
     def __init__(self):
@@ -181,10 +281,10 @@ class PredictResult:
         print("False negative : " + str(self.false_negative))
 
 
-machine = Machine()
+machine = DeepLearningMachine()
 with open('data/trn.txt') as file:
     machine.learnFile(file)
 
 test_datas = []
 with open('data/tst.txt') as file:
-    machine.predictFile(file)
+    machine.predictFile(file, False)
